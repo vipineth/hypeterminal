1386334e8800bfbf53e2b1aba2de0c066b9f0e89
vipineth <vipineth@gmail.com>
2026-02-04 21:57:08 +0530

    refactor: update font families to use IBM Plex Sans for consistency across styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -26,7 +26,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500;600;700&display=swap"
+					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&display=swap"
 					rel="stylesheet"
 				/>
 				<HeadContent />
